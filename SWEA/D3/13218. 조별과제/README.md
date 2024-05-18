a671285eaeeec6007af40a3b8478080dd556c4e4
sinheyy <163747140+sinheyy@users.noreply.github.com>
2024-05-18 18:27:48 +0900

    [D3] Title: 조별과제, Time: 116 ms, Memory: 24,588 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/README.md
@@ -0,0 +1,15 @@
+# [D3] 조별과제 - 13218 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXzjvCCq-PwDFASs) 
+
+### 성능 요약
+
+메모리: 24,588 KB, 시간: 116 ms, 코드길이: 565 Bytes
+
+### 제출 일자
+
+2024-05-18 18:27
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/13218. 조별과제/조별과제.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution {
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] test = br.readLine().split(" ");
+        int T = Integer.parseInt(test[0]); // test case
+         
+        for (int t = 1;t <= T;t++) {
+        	int N = Integer.parseInt(br.readLine());
+
+        	System.out.println("#"+t+" "+N/3);
+        }
+         
+    }
+ 
+}
