370cf123e725537b3909b61cca75549ab95c16ea
sinheyy <163747140+sinheyy@users.noreply.github.com>
2026-08-28 15:09:29 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,18 +2,16 @@ class Solution {
     public boolean solution(String s) {
         boolean answer = true;
         
+        if ((s.length() != 4) && (s.length() != 6)){
+            return false;
+        }
+        
         for (int i = 0;i < s.length();i++) {
-            if ((s.length() != 4) && (s.length() != 6)){
-                answer = false;
-                break;
-            }
-            
             if((s.charAt(i) - '0') > 9 || (s.charAt(i) - '0') < 0) {
-               answer = false;
-               break;
+               return false;
            }
         }
         
-        return answer;
+        return true;
     }
 }
